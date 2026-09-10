26486ea8f0d5d90c8fdf5d22f923ca407c9e09c7
serverperhema-collab <serverperhema@gmail.com>
2026-09-10 08:14:57 +0200

    Update route.ts

--- src/app/api/tl/wizard-trattativa/route.ts
+++ src/app/api/tl/wizard-trattativa/route.ts
@@ -57,6 +57,7 @@ export async function POST(req: Request) {
           contactId,
           status: trattativaStatus,
           currentOperatorId: operatorId,
+          createdByOperatorId: operatorId,
           
           currentCommercialeId: commercialeId || null,
           nextActionType: nextActionType,
